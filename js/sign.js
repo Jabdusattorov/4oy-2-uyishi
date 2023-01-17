c41e60cb49548f1043174725a3ea619d014d96d1
6 - uy ishi <jaloliddinabdusattorov83@gmail.com>
2023-01-17 14:07:47 +0500

    trewq

--- js/sign.js
+++ js/sign.js
@@ -17,7 +17,39 @@ signup.addEventListener("click", ()=>{
     localStorage.setItem("password", inp_password.value)
     inp_password.value = localStorage.getItem("name")
 
-    location.window("./pages/index.html")
+    window.location.replace("./pages/index.html");
 })
 
 console.log(inp_username.value);
+
+
+const inp_usernames = document.querySelector("#username")
+const inp_passwords = document.querySelector("#pass")
+
+if(inp_usernames.value == localStorage.getItem("username") && inp_passwords == localStorage.getItem("password")){
+    function style(){
+        document.getElementsByClassName("div").style.backgroundColor = "red";
+        document.getElementsByClassName("div").style.zIndex = "-1";
+    }
+}
+
+const loginBtn = document.querySelector(".login")
+const signBtn = document.querySelector(".sign")
+const formLogin = document.querySelector("#login")
+const formSignUp = document.querySelector("#signup")
+
+loginBtn.addEventListener("click", ()=>{
+    formSignUp.style.display = "none"
+    formLogin.style.display = "block"
+    
+    loginBtn.style.color = "rgba(18, 167, 251, 1)"
+    signBtn.style.color = "white"
+})
+
+signBtn.addEventListener("click", ()=>{
+    formLogin.style.display = "none"
+    formSignUp.style.display = "block"
+
+    signBtn.style.color = "rgba(18, 167, 251, 1)"
+    loginBtn.style.color = "white"
+})
